ffdfb6a3b6cec9b684b0b9bf69f3505743253253
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-15 05:42:17 -0400

    Fix deprecation of `ReflectionMethod::setAccessible()` in `TestCase` by conditionally revoking accessibility based on PHP version.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -259,7 +259,9 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
         }
 
         $result = $property->getValue($object);
-        if ($revoke) {
+
+        // @link https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_reflectionsetaccessible
+        if ($revoke && PHP_VERSION_ID < 80500) {
             $property->setAccessible(false);
         }
 

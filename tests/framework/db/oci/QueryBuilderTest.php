9c065009afcddc3eb0ebfcaccf80215a0481a2a8
Wilmer Arambula <terabytesoftw@gmail.com>
2025-10-22 10:44:05 -0300

    Fixed tests.

--- tests/framework/db/oci/QueryBuilderTest.php
+++ tests/framework/db/oci/QueryBuilderTest.php
@@ -366,7 +366,12 @@ WHERE rownum <= 1) "EXCLUDED" ON ("T_upsert"."email"="EXCLUDED"."email") WHEN NO
          */
         try {
             $encodedBackslash = substr($this->getDb()->quoteValue('\\'), 1, -1);
+
             $this->likeParameterReplacements[$encodedBackslash] = '\\';
+
+            foreach ($expectedParams as $name => $value) {
+                $expectedParams[$name] = strtr($value, $this->likeParameterReplacements);
+            }
         } catch (Exception $e) {
             $this->markTestSkipped('Could not execute Connection::quoteValue() method: ' . $e->getMessage());
         }
